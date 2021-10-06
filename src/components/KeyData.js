31a5de81e68abc6adedc13a5e6738dc04e65b40e
Erwan de Belleville <edebelleville@hotmail.fr>
2021-10-06 20:55:26 +0200

    fix bug

--- src/components/KeyData.js
+++ src/components/KeyData.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropType from 'prop-types';
 import calorieIcon from '../public/medias/calories-icon.png';
 import proteinIcon from '../public/medias/protein-icon.png';
 import carbsIcon from '../public/medias/carbs-icon.png';
 import fatIcon from '../public/medias/fat-icon.png';
 
-
-
+/**
+* This component view show keyData 
+* Data come from the page Home.js
+* 
+* @author Erwan
+* @version 1.1
+*/
 
 const KeyData = ({ user }) => {
+
+    if(!user.keyData) {
+        return null
+    }
 
     return (
         <div className ="keyData">
@@ -15,7 +25,7 @@ const KeyData = ({ user }) => {
             <div className="keyCount">
                     <img src={calorieIcon} alt="Icone flamme rouge"/>
                     <div className="caloriesCountData">
-                        <p className="dataBold">1900kCal</p>
+                        <p className="dataBold">{user.keyData.calorieCount}kCal</p>
                         <p>Calories</p>
                     </div>
             </div>
@@ -23,7 +33,7 @@ const KeyData = ({ user }) => {
             <div className="keyCount">
                     <img src={proteinIcon} alt="Icone cuisse poulet bleu"/>
                     <div className="proteilCountData">
-                        <p className="dataBold">140g</p>
+                        <p className="dataBold">{user.keyData.proteinCount}g</p>
                         <p>Proteines</p>
                     </div>
             </div>
@@ -31,7 +41,7 @@ const KeyData = ({ user }) => {
             <div className="keyCount">
                     <img src={carbsIcon} alt="Icone pommes jaunes"/>
                     <div className="carbohydrateCountData">
-                        <p className="dataBold">250g</p>
+                        <p className="dataBold">{user.keyData.carbohydrateCount}g</p>
                         <p>Glucides</p>
                     </div>
             </div>
@@ -39,7 +49,7 @@ const KeyData = ({ user }) => {
             <div className="keyCount">
                     <img src={fatIcon} alt="Icone flamme rouge"/>
                     <div className="lipidCountData">
-                        <p className="dataBold">50g</p>
+                        <p className="dataBold">{user.keyData.lipidCount}g</p>
                         <p>Lipides</p>
                     </div>
             </div>
@@ -47,5 +57,12 @@ const KeyData = ({ user }) => {
         </div>
     );
 };
+
+/*
+KeyData.propTypes ={
+    firstname:PropTypes.string.isRequired
+}
+*/
+
 
 export default KeyData;
--- src/components/WelcomeUser.js
+++ src/components/WelcomeUser.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+
+/**
+* This component view show user name
+* Data come from the page Home.js
+* 
+* @param user is props from home
+*
+* @author Erwan
+* @version 1.1
+*/
+
 
 const WelcomeUser = ({ user }) => {
+
+    if(!user.userInfos){
+        return null
+    }
     console.log(user);
     return (
         <div className="accueil-display">
@@ -9,5 +25,10 @@ const WelcomeUser = ({ user }) => {
         </div>
     );
 };
+
+
+WelcomeUser.propTypes ={
+    firstname:PropTypes.string.isRequired
+}
 
 export default WelcomeUser;
