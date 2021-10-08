62cf48ea56098d07d3287a19a19d867a38db0cb0
Erwan de Belleville <edebelleville@hotmail.fr>
2021-10-08 15:54:27 +0200

    ajout style

--- src/components/DailyScore.js
+++ src/components/DailyScore.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
 /**
 * This component render a radar chart with performance activity
 * Data come from state Home.js
@@ -15,10 +15,34 @@ const DailyScore = ({ user }) => {
     if(!user.todayScore) {
         return null
     }
+    console.log(user.todayScore)
+
+   
 
     return (
         <div className="dailyScoreChart">
-            
+            <ResponsiveContainer width="100%" height="100%">
+                <RadialBarChart 
+                    cx="50%" 
+                    cy="50%" 
+                    innerRadius="10%" 
+                    outerRadius="80%" 
+                    barSize={10} 
+                    data={user}>
+                    <RadialBar
+                        minAngle={15}
+                        label={{ position: 'insideStart', fill: '#fff' }}
+                        background
+                        clockWise
+                        dataKey="todayScore"
+                    />
+                    <Legend 
+                        iconSize={10} 
+                        layout="vertical" 
+                        verticalAlign="middle" 
+                    />
+                </RadialBarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
